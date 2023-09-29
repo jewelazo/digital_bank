acf8fd2fcc6e7c62c1e76dc3a0df400b0e8caaa5
jewelazo <giancarlopalominoe@uni.pe>
2023-09-29 12:58:55 -0500

    initial configuration

--- digital_bank/asgi.py
+++ digital_bank/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digital_bank.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digital_bank.settings.local')
 
 application = get_asgi_application()
