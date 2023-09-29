b7e6616a9a6080ebc1962bb32335889237a94134
jewelazo <giancarlopalominoe@uni.pe>
2023-09-29 13:16:15 -0500

    add postgresql as database

--- digital_bank/settings/local.py
+++ digital_bank/settings/local.py
@@ -4,7 +4,11 @@ from .base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DATABASE_NAME', default='postgres'),
+        'USER': env('DATABASE_USER', default='postgres'),
+        'PASSWORD': env('DATABASE_PASSWORD', default='none'),
+        'HOST': env('DATABASE_HOST', default='localhost'),
+        'PORT': '5432',
     }
 }
